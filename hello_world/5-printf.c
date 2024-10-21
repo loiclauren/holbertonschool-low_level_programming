c93e0443c35234f2779e9b37e0c08a1e31c89c6d
loiclauren <loiclauren97@gmail.com>
2024-10-21 12:24:51 +0000

    kaka

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "with proper grammar, but the outcome
+ * is a piece of art," followed by a new line to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
