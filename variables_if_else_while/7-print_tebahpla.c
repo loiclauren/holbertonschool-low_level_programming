2ce4c204192363457f43bfc027dbfe095aa5a0e9
loiclauren <loiclauren97@gmail.com>
2024-10-24 09:44:58 +0000

    loic

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse order,
+ *             followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
