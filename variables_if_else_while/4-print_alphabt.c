25737a00da2e7e295e8c5a51950d47fcbb8443e7
loiclauren <loiclauren97@gmail.com>
2024-10-24 09:19:16 +0000

    loic

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase except for 'q' and 'e',
+ *             followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}fuck me
+putchar('\n');
+return (0);
+}
+
+
+
+
+
+
+
