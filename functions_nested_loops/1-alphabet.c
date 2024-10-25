a95c2acb79dbd061b93b951a9eef5b0bcc86b6a9
loiclauren <loiclauren97@gmail.com>
2024-10-25 07:31:27 +0000

    loic

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "_putchar\n", 9);
+return (0);
+}
