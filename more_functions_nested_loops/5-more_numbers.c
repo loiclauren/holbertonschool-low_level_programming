76419b39c1830cc60254099afe802944cda7568f
loiclauren <loiclauren97@gmail.com>
2024-10-30 13:54:07 +0000

    loic

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+int n, m;
+for (m = 0; m < 10; m++)
+{
+for (n = 0; n < 15; n++)
+{
+if (n > 9)
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+_putchar('\n');
+}
+}
